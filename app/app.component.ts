ee6b746179bd746abcec9c9fae811636d1a2ea3c
Trisha Corpuz <trish@smartwave.ph>
2017-03-09 15:29:36 +0800

    test

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
 
+import '../style/app.scss';
+
 @Component({
-    selector: 'my-app',
-    styles: [`h1 {
-	color: white;
-	background: darkgray;
-	padding: 20px;
-}
-`],
-    template: `
-<main>
-  <h1>AV Timelogs!</h1>
-
-  <!-- Images (and assets) are parsed and loaded from within the public directory -->
-  <img src="/img/timelogicon.png">
-
-  <router-outlet></router-outlet>
-</main>
-
-<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>`,
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-
+export class AppComponent {
+  url = 'https://google.com';
+  title: string;
+}
